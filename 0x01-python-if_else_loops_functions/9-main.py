81a006416b91372c1518444e08b5a21af4de683a
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-09-21 19:10:33 +0000

    Funtions

--- 0x01-python-if_else_loops_functions/9-main.py
+++ 0x01-python-if_else_loops_functions/9-main.py
@@ -5,3 +5,4 @@ print_last_digit(98)
 print_last_digit(0)
 r = print_last_digit(-1024)
 print(r)
+
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,4 +2,4 @@
 def print_last_digit(number):
     last = abs(number) % 10
     print(last, end="")
-    return 
+    return last
