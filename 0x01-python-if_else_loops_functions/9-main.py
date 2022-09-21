c53497d56e436aad5a9405b8f93ad1ddae9ed5d7
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-09-21 19:04:31 +0000

    Funtions

--- 0x01-python-if_else_loops_functions/9-main.py
+++ 0x01-python-if_else_loops_functions/9-main.py
@@ -1,4 +1,5 @@
-st_digit = __import__('9-print_last_digit').print_last_digit
+#!/usr/bin/env python3
+print_last_digit = __import__('9-print_last_digit').print_last_digit
 
 print_last_digit(98)
 print_last_digit(0)
