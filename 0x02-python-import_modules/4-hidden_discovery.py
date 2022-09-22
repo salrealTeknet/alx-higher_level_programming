dbc906737c58185d23664f07a2790c3045a75947
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-09-22 20:09:20 +0000

    import

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
