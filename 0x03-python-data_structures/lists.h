4ad58683d511ed54135eeb9a793b64a315f59b54
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-09-26 14:06:47 +0000

    structure

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -7,12 +7,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -20,5 +20,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+listint_t *reverse_list(listint_t **head);
 
 #endif /* LISTS_H */
