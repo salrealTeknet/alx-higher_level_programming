fa6dd92f6a47124441576553b6b34aacae85e9e9
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-11-29 15:50:56 -0800

    MySQL

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
--- script that lists all records with a score >= 10 in second_table
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
