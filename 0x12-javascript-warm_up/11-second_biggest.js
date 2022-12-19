24246bf3301e9b1976b5022a263a2c07ad55d979
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-12-19 11:42:11 -0800

    Javascript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments.
-
-if (process.argv.length <= 3) {
+// script that searches the second biggest integer in the list of arguments
+const mySol = process.argv;
+if (mySol.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  console.log(mySol.sort((x, y) => y - x).slice(3)[0]);
 }
