28787236a6b2c8db3d322a1f9a43d4ed9594ccef
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-12-20 16:32:46 -0800

    Java

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-let dataFile1 = '';
-let dataFile2 = '';
-//  Gathering data 📀
-try {
-  dataFile1 = fs.readFileSync(process.argv[2], 'utf8');
-  dataFile2 = fs.readFileSync(process.argv[3], 'utf-8');
-} catch (err) {
-  console.error(err);
-}
-// Writting file 📂
-const writeToFile = (data) => {
-  fs.appendFile(process.argv[4], data + '\n', (err) => {
-    if (err) {
-      return console.error(err);
-    }
-  });
-};
-writeToFile(dataFile1);
-writeToFile(dataFile2);
+const first = fs.readFileSync(process.argv[2], 'utf8');
+const second = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], first + second);
